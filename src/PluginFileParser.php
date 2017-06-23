7f045df0fe167097d6cb774b23610ac08a0adde6
Andrew Feeney <webspannerdev@gmail.com>
2017-06-23 14:12:32 +1000

    Changelog
    - Add plugin file parser to get plugin version
    - Improve output of command line task

---
+++ src/PluginFileParser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MakeWeb\Shipper;
+
+class PluginFileParser
+{
+    public function getPluginVersion($filename)
+    {
+        return $this->getPluginAttribute($filename, 'Version');
+    }
+
+    public function getPluginName($filename)
+    {
+        return $this->getPluginAttribute($filename, 'Plugin Name');
+    }
+
+    public function getPluginAttribute($filename, $attribute)
+    {
+        $versionLine = array_filter($this->getPluginData($filename), function($line) use ($attribute) {
+            return $this->stringContains($line, $attribute.':');
+        });
+
+        $versionLine = reset($versionLine);
+
+        return trim(str_replace('*', '', str_replace($attribute.':', '', $versionLine)));
+    }
+
+    public function getPluginData($filename)
+    {
+        return explode("\n", file_get_contents($filename));
+    }
+
+    /**
+     * Determine if a given string contains a given substring.
+     *
+     * @param  string  $haystack
+     * @param  string|array  $needles
+     * @return bool
+     */
+    public function stringContains($haystack, $needles)
+    {
+        foreach ((array) $needles as $needle) {
+            if ($needle != '' && mb_strpos($haystack, $needle) !== false) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/Shipper.php
+++ src/Shipper.php
@@ -10,9 +10,12 @@ class Shipper
 
     protected $zipper;
 
+    protected $pluginFileParser;
+
     public function __construct()
     {
         $this->fileManager = new FileManager;
+        $this->pluginFileParser = new PluginFileParser;
         $this->zipper = new Zipper;
     }
 
@@ -24,6 +27,15 @@ class Shipper
     {
         $this->dir = $dir;
 
+        $this->version = $this->getVersion();
+
+        $this->output('');
+        $this->output('Preparing ', false);
+        $this->green($this->getPluginName(), false);
+        $this->output(' version ', false);
+        $this->green($this->version, false);
+        $this->output(' for shipping.');
+
         // Get the config files
         $this->configFilePath = $this->fileManager->getHomeDirectory().'/.makeweb/config.php';
 
@@ -32,42 +44,62 @@ class Shipper
 
         $releaseDirectory = $shippedPluginDirectory.'/'.$this->baseDirectory();
         $finalDestination = $releaseDirectory.'/'.$this->baseDirectory();
+        $finalZipPath = $finalDestination.'-'.$this->version.'.zip';
+
+        $this->output('');
+        $this->output('Zip file will be saved to ', false);
+        $this->green($finalZipPath);
+        $this->output('');
 
         // Create the shipped plugin directory if it does not yet exist
         if (!file_exists($shippedPluginDirectory)) {
+            $this->output('Creating directory: '.$shippedPluginDirectory);
             mkdir($shippedPluginDirectory);
         }
 
         // Create the plugin release directory if it does not yet exist
         if (!file_exists($releaseDirectory)) {
+            $this->output('Creating directory: '.$releaseDirectory);
             mkdir($releaseDirectory);
         }
 
         // Copy the files to their shipped location
+        $this->output('Copying plugin files to: '.$finalDestination);
         $this->fileManager->xcopy($dir, $finalDestination);
 
         // Remove studio.json file if it exists
         if (file_exists($finalDestination . '/studio.json')) {
+            $this->output('Removing file: '. $finalDestination . '/studio.json');
             unlink($finalDestination . '/studio.json');
         }
 
         // Run composer install
+        $this->output('Running composer install');
         echo shell_exec("rm -R $finalDestination/vendor");
         echo shell_exec("composer install --prefer-dist --no-plugins --no-dev -d $finalDestination --ansi");
 
         // Delete skipped files
+        $this->output('Deleting files named in .shipignore file');
         foreach($this->getSkippedFiles() as $filename) {
             if (empty($filename)) {
                 continue;
             }
             $path = $finalDestination . '/' . $filename;
+            $this->output('Deleting: '.$path);
             echo shell_exec("rm -Rf $path");
         }
 
-        $this->zipper->zipDirectory($finalDestination, $finalDestination.'-'.$this->getVersion().'.zip');
+        $this->output('Zipping up remaining files');
+        $this->zipper->zipDirectory($finalDestination, $finalZipPath);
 
         // Delete the unzipped directory
+        $this->output('Deleting the build directory');
         shell_exec("rm -Rf $finalDestination");
+
+        $this->output('');
+        $this->green('Success!');
+        $this->output('');
+        $this->green($finalZipPath.' ready to ship!');
     }
 
     protected function getConfig()
@@ -100,11 +132,40 @@ class Shipper
 
     protected function getVersion()
     {
-        return "1.0.0";
+        return $this->pluginFileParser->getPluginVersion($this->pluginFilePath());
+    }
+
+    protected function getPluginName()
+    {
+        return $this->pluginFileParser->getPluginName($this->pluginFilePath());
     }
 
     protected function baseDirectory()
     {
         return basename($this->dir);
+    }
+
+    protected function pluginFilePath()
+    {
+        return $this->dir.'/'.$this->baseDirectory().'.php';
+    }
+
+    protected function output($string, $lineBreak = true, $colorCode = '37')
+    {
+        echo("\033[{$colorCode}m$string\033[0m");
+
+        if ($lineBreak) {
+            echo("\n");
+        }
+    }
+
+    protected function green($message, $lineBreak = true)
+    {
+        $this->output($message, $lineBreak, '32');
+    }
+
+    protected function cyan($message, $lineBreak = true)
+    {
+        $this->output($message, $lineBreak, '36');
     }
 }
